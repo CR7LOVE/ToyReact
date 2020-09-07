4ca31e0c04f2f10e61b6c45b4c731ac1932c98ac
张灿 <394203959@qq.com>
2020-09-07 22:36:37 +0800

    [第一节课内容：JSX 的原理和关键实现]

---
+++ README.md
@@ -0,0 +1,22 @@
+第一节课内容：JSX 的原理和关键实现。  
+思路步骤：
+1. 使用 webpack 的 pulgin(@babel/plugin-transform-react-jsx) 在 dist 中看到了 jsx 被编译成了 React.createElement，所以要想着怎样实现 React.createElement,
+React.createElement 只是个名字，可以换成自己的，代码中换成了 createElement
+2. 把 1 中的代码所生成的 node 加载到 document.body 上
+3. createElement 目前只能接收原生标签，开始写自定义组件，其中包含了封装 Component 基类, ElementWrapper 和 TextWrapper
+4. 最张效果是 class MyComponent extends Component ,并把结果添加到了 document.body 上  
+
+或者这样描述，为了实现 JSX：
+1. 需要知道 JSX 最终被编译成了什么，这个可以通过 webpack 的 babel plugin 知道。  
+2. 1 的编译结果是 React.createElement，可以把这个换个名字，所以，实现 React.createElement 即可，并挂载到 DOM 上
+3. 在 2 的基础上实现自定义组件，并挂载到 DOM 上
+
+具体执行过程：  
+1. MyComponent 的 3 个 children 先执行 createElement()；  
+2. MyComponent 的实例被初始化，constructor 中的 props 属性上添加 id 与 class 属性；  
+3. MyComponent 的实例的 constructor 中的 children 属性上添加 1 的 3 个 div
+4. MyComponent 的实例的 constructor 中的 get root 开始执行，get root() 中的 this.render().root 中的 render 开始执行
+5. render() 的结果是个 JSX，即 createElement，所以进入 createElement，此时是 h1
+6. 接 5，再来一次 createElement，此时是 MyComponent 的外壳 div，里面包含了 h1 和 3 个 div，此时 get root 执行完毕，拿到的结果是外壳 div 加个里面的 4 个元素，即自定义元素返回的元素已经拿到了
+7. 添加到 document.body 中
+
---
+++ toy-react.js
@@ -0,0 +1,70 @@
+class ElementWrapper {
+    constructor(type) {
+        this.root = document.createElement(type);
+    }
+    setAttribute(name, value) {
+        this.root.setAttribute(name, value)
+    }
+    appendChild(component) {
+        this.root.appendChild(component.root); // 注意这里是 component.root
+    }
+}
+
+class TextWrapper {
+    constructor(content) {
+        this.root = document.createTextNode(content);
+    }
+}
+
+export class Component {
+    constructor() {
+        this.props = Object.create(null);
+        this.children = [];
+        this._root = null;
+    }
+    setAttribute(name, value) {
+        this.props[name] = value;
+    }
+    appendChild(component) {
+        this.children.push(component) // 注意这里就没有 .root，上面 L9 有 .root 是因为一个节点要添加另一个节点，所以是 .root appendChild .root，而这里只是存到自己的数组中，所以不用 .root
+    }
+    get root() {
+        if(!this._root) {
+            this._root = this.render().root; // render 指的是 自定义组件中的 render，render() 的结果是 JSX，JSX 会被编译成 React.createElement()，所以这里又会调用 createElement,
+        }
+        return this._root;
+    }
+}
+
+// 源码中不是这样子的，这样写方便理解
+export function createElement(type, attributes, ...children) {
+    let e;
+    if(typeof type === 'string') { // 这是原生标签的情况
+        e = new ElementWrapper(type);
+    } else {
+        e = new type(); // 这是组件的情况
+    }
+
+    for (let p in  attributes) {
+        e.setAttribute(p, attributes[p]);
+    }
+    let insertChildren = (children) => {
+        for (let child of children) {
+            if(typeof child === 'string') {
+                child = new TextWrapper(child);
+            }
+            if(typeof child === 'object' && child instanceof Array) { // this.children 的情况
+                insertChildren(child)
+            } else {
+                e.appendChild(child) // 自定义组件添加 child 的情况
+            }
+        }
+    };
+
+    insertChildren(children);
+    return e;
+}
+
+export function render(component, parentElement) {
+    parentElement.appendChild(component.root);
+}
